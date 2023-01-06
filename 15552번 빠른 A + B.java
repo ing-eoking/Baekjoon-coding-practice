9a1a776bfb8ca1d07329fca3d125a53b1e925212
Yeoncheol-Kim <koo05131@yahoo.co.kr>
2023-01-06 11:53:46 +0900

    Create 15552번 빠른 A + B.java

---
+++ 15552번 빠른 A + B.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(bf.readLine());
+        for(int i =0; i < T; i++){
+            st = new StringTokenizer(bf.readLine());
+            bw.write(Integer.toString(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+"\n");
+        }
+        bw.close();
+    }
+}
