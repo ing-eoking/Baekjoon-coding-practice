c28a46b7905d0aa496edbe47edeacf687971f941
Yeoncheol-Kim <koo05131@yahoo.co.kr>
2023-01-14 13:18:20 +0900

    Create 1463번 1로 만들기.cpp

---
+++ 1463번 1로 만들기.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define MIN(a,b) a > b ? b : a
+
+int main() {
+	int N;
+	scanf("%d", &N);
+	vector<int> v(N+2);
+	v[3] = v[2] = 1;
+	for (int i = 4; i <= N; i++) {
+		int A = v[i - 1] + 1;
+		if (i % 3 == 0) A = MIN(A, v[i / 3] + 1);
+		if (i % 2 == 0) A = MIN(A, v[i / 2] + 1);
+		v[i] = A;
+	}
+
+	printf("%d\n", v[N]);
+}
