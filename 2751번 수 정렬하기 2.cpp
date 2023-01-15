a7c99a8c50081d8fedbbef3bb10c8754b22c8e57
Yeoncheol-Kim <koo05131@yahoo.co.kr>
2023-01-15 13:29:34 +0900

    Create 2751번 수 정렬하기 2.cpp

---
+++ 2751번 수 정렬하기 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main() {
+	int N, t=0;
+	scanf("%d", &N);
+	set<int> s;
+	while (N--) {
+		scanf("%d", &t);
+		s.insert(t);
+	}
+	for (auto i : s)
+		printf("%d\n", i);
+
+}
