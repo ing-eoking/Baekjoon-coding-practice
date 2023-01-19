06c48ccb795341aa2b6710c469b78bb3ad305b61
Yeoncheol-Kim <koo05131@yahoo.co.kr>
2023-01-19 13:23:51 +0900

    Create 1676번 팩토리얼 0의 개수.java

---
+++ 1676번 팩토리얼 0의 개수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(bf.readLine());
+        bw.write(Integer.toString(N/5 + N/25 + N/125) + "\n");
+        bw.close();
+    }
+}
