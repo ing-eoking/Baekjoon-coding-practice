b3d4d777a1e91d58419ac5acd7e685dc0b7879bb
Yeoncheol-Kim <koo05131@yahoo.co.kr>
2023-02-10 18:27:19 +0900

    Create 24444번 알고리즘 수업 - 너비 우선 탐색 1.cpp

---
+++ 24444번 알고리즘 수업 - 너비 우선 탐색 1.cpp
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <bitset>
+#include <map>
+#include <set>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+bitset<100000> visited;
+map<int, set<int>> m;
+vector<int> ord;
+int cur = 1;
+
+void bfs(int r) {
+	visited.reset();
+	visited[r] = 1;
+	queue<int> q;
+	q.push(r);
+	int u;
+	while (!q.empty()) {
+		u = q.front();
+		q.pop();
+		ord[u] = cur++;
+		for(int v : m[u])
+			if (visited[v] == 0) {
+				visited[v] = 1;
+				q.push(v);
+			}
+	}
+}
+
+int main()
+{
+	int N, M, R, u, v;
+	scanf("%d%d%d", &N, &M, &R);
+	ord = vector<int>(N + 1);
+	for (int i = 0; i < M; i++) {
+		scanf("%d%d", &u, &v);
+		m[v].insert(u);
+		m[u].insert(v);
+	}
+	bfs(R);
+	for (int i = 1; i <= N; i++)
+		printf("%d\n", ord[i]);
+}
